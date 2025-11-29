9e709e6e1119c0550f5fd18be1236874b59ec803
Muhammad Irfan Irsyad <irsyadcahkalipucang@gmail.com>
2025-11-29 10:54:02 +0700

    add landing page

---
+++ app/components/app/nav-bar.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="navbar bg-primary text-primary-content">
+    <div class="navbar-start">
+      <NuxtLink to="/" class="btn btn-ghost text-xl">
+        Catalog App
+      </NuxtLink>
+    </div>
+    <div class="navbar-end">
+      <button class="btn btn-neutral" @click="navigateTo('/seller/auth/register')">
+        Daftar Seller
+      </button>
+    </div>
+  </div>
+</template>
---
+++ app/pages/index.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="hero bg-base-300 container mx-auto rounded-lg mt-10">
+    <div class="hero-content text-center min-h-96">
+      <div class="max-w-md">
+        <h1 class="text-5xl font-bold">
+          Catalog App
+        </h1>
+        <p class="py-6">
+          Aplikasi katalog produk sederhana untuk para seller memamerkan produk
+          mereka kepada calon pembeli.
+        </p>
+        <button class="btn btn-primary" @click="navigateTo('/seller/auth/register')">
+          Daftar seller <Icon name="tabler:arrow-right" size="24" />
+        </button>
+      </div>
+    </div>
+  </div>
+</template>
