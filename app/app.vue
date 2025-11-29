a50530d7756b7a7c4fbe3eb5606326b7975e4ea8
Muhammad Irfan Irsyad <irsyadcahkalipucang@gmail.com>
2025-11-29 10:17:33 +0700

    setup tailwindcss / daisyui / icon

--- app/app.vue
+++ app/app.vue
@@ -1,6 +1,10 @@
 <template>
   <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
+    <h1 class="text-3xl font-bold underline">
+      Hello world!
+    </h1>
+    <button class="btn btn-primary">
+      Button
+    </button>
   </div>
 </template>
